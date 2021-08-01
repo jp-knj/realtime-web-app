46b89d794e0233017c4814f4316b446c4e2c964e
jp-knj <kenji.tomita1996@gmail.com>
2021-08-01 10:43:07 +0900

    :sparkles: Create WebSocket

--- 01_session/todo-ws/app.js
+++ 01_session/todo-ws/app.js
@@ -15,7 +15,22 @@ const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 
 nextApp.prepare().then(
-  () => {},
+  () => {
+    // Next.jsのリクエストハンドラを引数にhttp.createServer()を実行
+    const server = http.createServer(nextApp.getRequestHandler()).listen(3000);
+
+    // WebSocket.Serverインスタンスを生成
+    const ws = new WebSocket.Server({ server });
+
+    // 接続中の全クライアントに現在のToDo一覧を送信する関数
+    function sendTodosToOpenClient() {
+      ws.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(todos));
+        }
+      });
+    }
+  },
   (err) => {
     console.error(err);
   }
