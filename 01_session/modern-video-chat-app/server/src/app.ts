54617363600ebff9be007a58a6090b39d9deb8ec
jp-knj <kenji.tomita1996@gmail.com>
2021-08-05 09:15:07 +0900

    :sparkles: Setup Server side

---
+++ 01_session/modern-video-chat-app/server/src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import config from "config";
+import logger from "./utils/logger";
+import { version } from "../package.json";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+
+const app = express();
+
+const httpServer = createServer(app);
+
+app.get("/", (_, res) =>
+  res.send(`Server is up and running version ${version}`)
+);
+
+httpServer.listen(port, host, () => {
+  logger.info(`🚀 Server version ${version} is listening`);
+  logger.info(`http://${host}:${port}`);
+});
