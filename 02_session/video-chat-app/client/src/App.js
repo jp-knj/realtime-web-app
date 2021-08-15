3012b7f0d8cfdc7ef1dcb84f9b69879dc15119ac
jp-knj <kenji.tomita1996@gmail.com>
2021-08-15 17:32:02 +0900

    :sparkles: Add react-router-dom

--- 02_session/video-chat-app/client/src/App.js
+++ 02_session/video-chat-app/client/src/App.js
@@ -1,12 +1,27 @@
 import React, { useEffect } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Dashboard from "./Dashboard/Dashboard";
+import Login from "./Login/Login";
 import { connectWithWebSocket } from "./utils/wssConnection/wssConnection";
 
 function App() {
   useEffect(() => {
     connectWithWebSocket();
   }, []);
-  return <div className="App">Hello React</div>;
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
