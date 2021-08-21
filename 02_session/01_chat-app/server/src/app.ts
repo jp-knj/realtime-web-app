fe7658af878970590c38c5df3d4d0c0f0f58d105
jp-knj <kenji.tomita1996@gmail.com>
2021-08-21 14:57:38 +0900

    :shirt: Create Logger

--- 02_session/01_chat-app/server/src/app.ts
+++ 02_session/01_chat-app/server/src/app.ts
@@ -3,6 +3,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import config from "config";
+import logger from "./utils/logger";
 
 const port = config.get<number>("port");
 const host = config.get<string>("host");
@@ -22,5 +23,5 @@ app.get("/", (_, res) => {
 });
 
 httpServer.listen(port, host, () => {
-  console.log("Server is listening");
+  logger.info("Server is listening");
 });
