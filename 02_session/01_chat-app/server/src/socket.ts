f7bb27965eed38652dd5ce66448173dd4e6c0384
jp-knj <kenji.tomita1996@gmail.com>
2021-08-22 15:04:44 +0900

    :+1: Add Events Obj

--- 02_session/01_chat-app/server/src/socket.ts
+++ 02_session/01_chat-app/server/src/socket.ts
@@ -1,9 +1,16 @@
 import { Server, Socket } from "socket.io";
 import logger from "./utils/logger";
 
+const EVENTS = {
+  connection: "connection",
+  CLIENT: {
+    CREATE_ROOM: "CREATE_ROOM",
+  },
+};
+
 function socket({ io }: { io: Server }) {
   logger.info("Socket enabled");
-  io.on("connection", (socket: Socket) => {
+  io.on(EVENTS.connection, (socket: Socket) => {
     logger.info(`user loggin ${socket.id}`);
   });
 }
