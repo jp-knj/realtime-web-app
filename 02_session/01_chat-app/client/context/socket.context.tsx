e64d1d67ba9c217177064c3a3da007b75e62baa4
jp-knj <kenji.tomita1996@gmail.com>
2021-08-22 14:28:26 +0900

    :+1: Create Username state and Add Login Func

--- 02_session/01_chat-app/client/context/socket.context.tsx
+++ 02_session/01_chat-app/client/context/socket.context.tsx
@@ -1,16 +1,27 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import io, { Socket } from "socket.io-client";
 import { SOCKET_URL } from "../config/default";
 
 interface Context {
   socket: Socket;
+  username?: string;
+  setUsername: Function;
 }
 
 const socket = io(SOCKET_URL);
-const SocketContext = createContext<Context>({ socket });
+const SocketContext = createContext<Context>({
+  socket,
+  setUsername: () => false,
+});
 
 const SocketsProvider = (props: any) => {
-  return <SocketContext.Provider value={{ socket }} {...props} />;
+  const [username, setUsername] = useState<string>("");
+  return (
+    <SocketContext.Provider
+      value={{ socket, username, setUsername }}
+      {...props}
+    />
+  );
 };
 
 export const useSockets = () => useContext(SocketContext);
--- 02_session/01_chat-app/client/pages/index.tsx
+++ 02_session/01_chat-app/client/pages/index.tsx
@@ -1,11 +1,26 @@
+import { useRef } from "react";
 import Messages from "../components/Messages";
 import Rooms from "../components/Rooms";
 import { useSockets } from "../context/socket.context";
 
 export default function Home() {
-  const { socket } = useSockets();
+  const { socket, username, setUsername } = useSockets();
+  const usernameRef = useRef(null);
+
+  function handleSetUsername() {
+    const value = usernameRef.current.value;
+    if (!value) return;
+    setUsername(value);
+    localStorage.setItem("username", value);
+  }
   return (
     <>
+      {!username && (
+        <div>
+          <input placeholder="Username" ref={usernameRef} />
+          <button onClick={handleSetUsername}>Start</button>
+        </div>
+      )}
       <Rooms />
       <Messages />
       <h2>{socket.id}</h2>
