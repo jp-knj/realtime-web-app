a77e7c3b5958d5e25b94628687896f57d00eb1a9
jp-knj <kenji.tomita1996@gmail.com>
2021-08-22 14:10:41 +0900

    :sparkles: Create New Component and show them

---
+++ 02_session/01_chat-app/client/components/Messages.tsx
@@ -0,0 +1,5 @@
+const Messages = () => {
+  return <p>Meeages</p>;
+};
+
+export default Messages;
--- 02_session/01_chat-app/client/pages/index.tsx
+++ 02_session/01_chat-app/client/pages/index.tsx
@@ -1,9 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Messages from "../components/Messages";
+import Rooms from "../components/Rooms";
 import { useSockets } from "../context/socket.context";
 
 export default function Home() {
   const { socket } = useSockets();
-  return <h1>{socket.id}</h1>;
+  return (
+    <>
+      <Rooms />
+      <Messages />
+      <h2>{socket.id}</h2>
+    </>
+  );
 }
