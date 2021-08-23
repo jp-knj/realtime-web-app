709ac511d807996e1b471a9d1b196f0453d47489
jp-knj <kenji.tomita1996@gmail.com>
2021-08-23 19:31:07 +0900

    :sparkles: get all rooms

--- 02_session/01_chat-app/server/src/socket.ts
+++ 02_session/01_chat-app/server/src/socket.ts
@@ -24,26 +24,19 @@ function socket({ io }: { io: Server }) {
   io.on(EVENTS.connection, (socket: Socket) => {
     logger.info(`user loggin ${socket.id}`);
 
+    socket.emit(EVENTS.SERVER.ROOMS, rooms);
+
     socket.on(EVENTS.CLIENT.CREATE_ROOM, ({ roomName }) => {
       console.log({ roomName });
-
-      // Create roomId
       const roomId = nanoid();
-
-      // add a new room to the room obj
       rooms[roomId] = {
         name: roomName,
       };
-      // join(roomId)
+
       socket.join(roomId);
 
-      // broadcast an event
-      socket.broadcast.emit(EVENTS.SERVER.ROOMS);
-
-      // emit back to the room creator with all the rooms
+      socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms);
       socket.emit(EVENTS.SERVER.ROOMS, rooms);
-
-      // emit event back the room creator
       socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
     });
 
@@ -52,7 +45,7 @@ function socket({ io }: { io: Server }) {
       ({ roomId, message, username }) => {
         const date = new Date();
 
-        socket.to(roomId).emit(EVENTS.CLIENT.SEND_ROOM_MESSAGE, {
+        socket.to(roomId).emit(EVENTS.SERVER.ROOM_MESSAGE, {
           message,
           username,
           time: `${date.getHours()}:${date.getMinutes()}`,
